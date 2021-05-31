d2d648fcaca7cdc3b5848a2af7c9360e9b5738b0
Lukasz Janusz <lukasz.t.janusz@gmail.com>
2021-06-01 01:15:57 +0200

    add randomNumber

--- js/script.js
+++ js/script.js
@@ -8,11 +8,14 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-let computerMove = `kamień`;
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to, ' + playerMove + ' to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
 
+let computerMove = 'nieznany ruch';
 
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
 
-
+printMessage('Mój ruch to: ' + computerMove);
