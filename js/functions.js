de5117e8fa815b51437d3d92fba4a0ef650355d1
Lukasz Janusz <lukasz.t.janusz@gmail.com>
2021-06-09 23:43:31 +0200

    add code formatting

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,50 @@
-   
-       function printMessage (msg) {
-	    let div = document.createElement ('div');
-	    div.innerHTML = msg;
-	    document.getElementById ('messages').appendChild(div);
-    }
+{
+       function printMessage(msg) {
+              let div = document.createElement('div');
+              div.innerHTML = msg;
+              document.getElementById('messages').appendChild(div);
+       }
 
-    function clearMessages () {
-	    document.getElementById('messages').innerHTML = '';
-    }
+       function clearMessages() {
+              document.getElementById('messages').innerHTML = '';
+       }
+
+       var playGame = function(playerInput) {
+              clearMessages();
+              
+              const getMoveName = function(argMoveId) {
+
+                     if (argMoveId == 1) {
+                            return 'kamień';
+                     } if (argMoveId == 2) {
+                            return 'papier';
+                     } if (argMoveId == 3) {
+                            return 'nozyce';
+                     } else {
+                            printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+                            return 'nieznany ruch';
+                     }
+              }
+
+              const randomNumber = Math.floor(Math.random() * 3 + 1);
+              const computerMove = getMoveName(randomNumber);
+              const playerMove = getMoveName(playerInput);
+
+              const displayResult = function(argComputerMove, argPlayerMove) {
+                     printMessage('Ruch komputera to: ' + argComputerMove + 'Twój ruch to: ' + argPlayerMove);
+                     
+                     if (
+                            (argComputerMove == 'kamień' && argPlayerMove == 'papier')
+                            || (argComputerMove == 'papier' && argPlayerMove == 'nozyce')
+                            || (argComputerMove == 'nozyce' && argPlayerMove == 'kamień')
+                     ) {
+                            printMessage('Ty wygrywasz!');
+                     } else if (argComputerMove == argPlayerMove) {
+                            printMessage('Remis! ')
+                     } else {
+                            printMessage('Tym razem przegrywasz :(');
+                     }
+              }
+              displayResult(computerMove, playerMove);
+       }
+}
