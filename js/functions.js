407cb9a4b5d565fa8759d77932433d7c060075dd
Lukasz Janusz <lukasz.t.janusz@gmail.com>
2021-06-14 00:00:29 +0200

    add comments

--- js/functions.js
+++ js/functions.js
@@ -9,42 +9,45 @@
               document.getElementById('messages').innerHTML = '';
        }
 
+       const getMoveName = function(argMoveId) {
+
+              if (argMoveId == 1) {
+                     return 'kamień';
+              } if (argMoveId == 2) {
+                     return 'papier';
+              } if (argMoveId == 3) {
+                     return 'nozyce';
+              } else {
+                     printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+                     return 'nieznany ruch';
+              }
+       }
+
+       const displayResult = function(argComputerMove, argPlayerMove) {
+              printMessage('Ruch komputera to: ' + argComputerMove + 'Twój ruch to: ' + argPlayerMove);
+              
+              if (
+                     (argComputerMove == 'kamień' && argPlayerMove == 'papier')
+                     || (argComputerMove == 'papier' && argPlayerMove == 'nozyce')
+                     || (argComputerMove == 'nozyce' && argPlayerMove == 'kamień')
+              ) {
+                     printMessage('Ty wygrywasz!');
+              } else if (argComputerMove == argPlayerMove) {
+                     printMessage('Remis! ')
+              } else {
+                     printMessage('Tym razem przegrywasz :(');
+              }
+       }
+       
        var playGame = function(playerInput) {
               clearMessages();
               
-              const getMoveName = function(argMoveId) {
-
-                     if (argMoveId == 1) {
-                            return 'kamień';
-                     } if (argMoveId == 2) {
-                            return 'papier';
-                     } if (argMoveId == 3) {
-                            return 'nozyce';
-                     } else {
-                            printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-                            return 'nieznany ruch';
-                     }
-              }
-
+             
               const randomNumber = Math.floor(Math.random() * 3 + 1);
               const computerMove = getMoveName(randomNumber);
               const playerMove = getMoveName(playerInput);
 
-              const displayResult = function(argComputerMove, argPlayerMove) {
-                     printMessage('Ruch komputera to: ' + argComputerMove + 'Twój ruch to: ' + argPlayerMove);
-                     
-                     if (
-                            (argComputerMove == 'kamień' && argPlayerMove == 'papier')
-                            || (argComputerMove == 'papier' && argPlayerMove == 'nozyce')
-                            || (argComputerMove == 'nozyce' && argPlayerMove == 'kamień')
-                     ) {
-                            printMessage('Ty wygrywasz!');
-                     } else if (argComputerMove == argPlayerMove) {
-                            printMessage('Remis! ')
-                     } else {
-                            printMessage('Tym razem przegrywasz :(');
-                     }
-              }
+              
               displayResult(computerMove, playerMove);
        }
 }
